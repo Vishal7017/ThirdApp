23c60a021f712a4fbaf15142209e5d9e3304d914
Vishal7017 <wavhalev@gmail.com>
2024-03-18 13:09:46 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Quiz App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const QuizHomePage(),
+    );
+  }
+}
+
+class QuizHomePage extends StatefulWidget {
+  const QuizHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<QuizHomePage> createState() => _QuizHomePageState();
+}
+
+class _QuizHomePageState extends State<QuizHomePage> {
+  int? operand1;
+  int? operand2;
+  int? result;
+  int totalAttempts = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 164, 242, 233),
+      appBar: AppBar(
+        title: Text('My Quiz'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Expression: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green[600],
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    operand1 == null ? '____' : '$operand1',
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    '+',
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    operand2 == null ? '____' : '$operand2',
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Solution: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green[600],
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    result == null ? '?' : '$result',
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Total Rounds: $totalAttempts',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  CupertinoButton(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    onPressed: () {
+                      setState(() {
+                        operand1 = Random().nextInt(100);
+                        operand2 = Random().nextInt(100);
+                        result = null;
+                        totalAttempts++;
+                      });
+                    },
+                    child: Text(
+                      totalAttempts == 0 ? 'Play' : 'Play Again',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    color: Colors.green[600],
+                  ),
+                  SizedBox(width: 10),
+                  CupertinoButton(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    onPressed: operand1 == null || operand2 == null
+                        ? null
+                        : () {
+                            setState(() {
+                              result = operand1! + operand2!;
+                            });
+                          },
+                    child: const Text(
+                      'Solve',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    color: Colors.green[600],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
